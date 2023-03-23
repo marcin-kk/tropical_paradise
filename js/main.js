cc18e73b936ea1575e46133d3791901e142b677e
Marcin Kula <mkula.poczta@gmail.com>
2023-03-23 17:41:05 +0100

    navigation done - links animated in JS

--- js/main.js
+++ js/main.js
@@ -10,6 +10,21 @@ const handleNav = () => {
 			nav.classList.remove('nav--active')
 		})
 	})
+
+	handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		
+        //animation-delay: .1s .2s .3s
+
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
 }
 
 navBtn.addEventListener('click', handleNav) //in scss we added transition on .nav so that the change is smooth
