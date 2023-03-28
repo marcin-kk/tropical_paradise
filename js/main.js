b999b8ad9ac98622b427e08c6b634b64bc6759f5
Marcin Kula <mkula.poczta@gmail.com>
2023-03-28 18:55:44 +0200

    burger menu color change functionality added in JS; date automation in JS added; PROJECT COMPLETE

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+
+	navBtnBars.classList.remove('black-bars-color')
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
@@ -19,12 +24,32 @@ const handleNavItemsAnimation = () => {
 
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
-		
-        //animation-delay: .1s .2s .3s
+
+		//animation-delay: .1s .2s .3s
 
 		item.style.animationDelay = '.' + delayTime + 's'
 		delayTime++
 	})
 }
 
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footYear.innerText = year
+}
+
+handleCurrentYear()
 navBtn.addEventListener('click', handleNav) //in scss we added transition on .nav so that the change is smooth
+
+window.addEventListener('scroll', handleObserver)
